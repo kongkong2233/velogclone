67d855d5320666723d05f3e520f7fa934edf7f4d
kongkong2233 <dbqls1703@naver.com>
2024-06-19 09:31:40 +0900

    0619 first commit

---
+++ src/main/java/org/example/velogclone/VelogcloneApplication.java
@@ -0,0 +1,13 @@
+package org.example.velogclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelogcloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VelogcloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/velogclone/VelogcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.velogclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelogcloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
